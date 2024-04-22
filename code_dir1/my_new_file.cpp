077ed277830c8f7e835f8ab1f96098ff112d1280
Deshmukh <atdeshmukh@ptc.com>
2024-04-22 14:37:46 +0530

    Created branch and added new file.

---
+++ code_dir1/my_new_file.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Some code for Demo.
+	cout<<"Hello World!"<<endl;
+	cout<<"This is Git Branching Demo."<<endl;
+	cout<<"Lets learn how branching and merging works."<<endl;
+	
+	return 0;
+}
