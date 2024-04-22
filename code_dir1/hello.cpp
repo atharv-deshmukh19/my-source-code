7406104154a0571c8599df5e3a6c89599b64c089
Deshmukh <atdeshmukh@ptc.com>
2024-04-22 14:16:13 +0530

    This is my first commit.

---
+++ code_dir1/hello.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Some code for Demo.
+	cout<<"Hello World!"<<endl;
+	cout<<"This is GitHub Demo."<<endl;
+	
+	return 0;
+}
---
+++ code_dir2/file2.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Some code for Demo.
+	cout<<"This file is from other directory."<<endl;
+	cout<<"This is GitHub Demo."<<endl;
+	
+	return 0;
+}
